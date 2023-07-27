ddf54d70bfcd59430e5a44d4b0e3459f807d0812
lee-orr <lee-orr@users.noreply.github.com>
2023-07-27 23:58:53 +0000

    setup colors
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- resumes/tailwind.config.cjs
+++ resumes/tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				text: "var(--color-text)",
+				background: "var(--color-background)",
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+			}
+		},
 	},
 	plugins: [],
 }
