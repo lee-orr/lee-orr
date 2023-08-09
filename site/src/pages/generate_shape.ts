7f4a410f6413bc1034c6857bf146babc9cb13580
lee-orr <lee-orr@users.noreply.github.com>
2023-08-09 22:18:15 +0000

    better animated backdrops
    Signed-off-by: lee-orr <lee-orr@users.noreply.github.com>

--- site/src/pages/generate_shape.ts
+++ site/src/pages/generate_shape.ts
@@ -1,8 +1,12 @@
 const OFFSETS = 0.03;
 const MIN = 0 - OFFSETS;
 const MAX = 1 + OFFSETS;
-const MID = (MAX - MIN) * 0.5 + MIN;
+const GAP = (MAX - MIN) * 0.5;
+const H_GAP = GAP / 2;
+const MID = GAP + MIN;
 const POLYGON: [number, number][] = [[MIN, MIN], [MID, MIN], [MAX, MIN], [MAX, MID], [MAX, MAX], [MID, MAX], [MIN, MAX], [MIN, MID]];
+const SPLAT_OFFSETS = 0.2;
+const SPLAT: [number, number][] = [[MIN + H_GAP, MIN + H_GAP], [MID, MIN], [MAX - H_GAP, MIN + H_GAP], [MAX, MID], [MAX - H_GAP, MAX - H_GAP], [MID, MAX], [MIN + H_GAP, MAX - H_GAP], [MIN, MID]];
 const print_point = ({ x, y }: { x: number; y: number; }): string => {
     return `${Math.floor(x * 100)}% ${Math.floor(y * 100)}%`;
 };
@@ -29,6 +33,23 @@ export const generate_shape = () => {
     let points: string[] = polygon.map(([x, y]) => {
         let x_offset = OFFSETS * Math.random();
         let y_offset = OFFSETS * Math.random();
+        return { x: x + x_offset, y: y + y_offset };
+    }).map(print_point);
+    let result = `polygon(${points.join(", ")})`;
+    return result;
+};
+
+
+
+export const generate_splat = () => {
+    let polygon = SPLAT;
+    let subdivide_count = Math.random() * 2;
+    for (let i = 0; i < subdivide_count; i++) {
+        polygon = subdivide(polygon);
+    }
+    let points: string[] = polygon.map(([x, y]) => {
+        let x_offset = SPLAT_OFFSETS * Math.random();
+        let y_offset = SPLAT_OFFSETS * Math.random();
         return { x: x + x_offset, y: y + y_offset };
     }).map(print_point);
     let result = `polygon(${points.join(", ")})`;
--- site/tailwind.config.cjs
+++ site/tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+			},
 			colors: {
 				text: colors("text", 900),
 				background: colors("secondary", 700),
