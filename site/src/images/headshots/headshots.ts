5ce8ec4dedb7a345e220e9f2a336614a75bed17f
Lee-Orr <leeorr@gmail.com>
2024-01-24 00:29:24 +0000

    fix headshots

---
+++ site/src/images/headshots/headshots.ts
@@ -0,0 +1,12 @@
+import i1 from "./DSC00084.jpg";
+import i2 from "./DSC00087.jpg";
+import i3 from "./DSC00091.jpg";
+import i4 from "./DSC00105.jpg";
+import i5 from "./DSC00159.jpg";
+import i6 from "./DSC00189.jpg";
+import i7 from "./DSC00197.jpg";
+import i8 from "./DSC00227.jpg";
+import i9 from "./DSC00241.jpg";
+import i10 from "./DSC00247.jpg";
+
+export const images = [i1, i2, i3, i4, i5, i6, i7, i8, i9, i10];
